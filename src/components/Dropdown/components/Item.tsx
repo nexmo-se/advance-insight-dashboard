058362e2512b6619840f6c8f7e23b30efd1eee50
enricop89 <enrico.portolan@gmail.com>
2021-03-18 09:19:26 +0000

    - clean dropdwon key

--- src/components/Dropdown/components/Item.tsx
+++ src/components/Dropdown/components/Item.tsx
@@ -5,13 +5,13 @@ import { Box } from "@material-ui/core";
 
 interface ItemProps {
   selected?: boolean;
-  value: string | null;
+  value: string;
   label?: string;
   key?: string;
   onClick?: (item: any) => void;
 }
 
-function Item ({ selected = false, value, key, label, onClick }: ItemProps) {
+function Item ({ selected = false, value, label, onClick }: ItemProps) {
   function handleClick (e: MouseEvent<HTMLDivElement>) {
     if (onClick) onClick({ value, label })
   }
@@ -25,7 +25,7 @@ function Item ({ selected = false, value, key, label, onClick }: ItemProps) {
         })
       }
       onClick={handleClick}
-      key={key}
+      
     >
       { label? label: value }
     </Box>
