2591963c03f272ab1df6109ff430ce7572c3e9dd
Frans Huang <franssiswanto@gmail.com>
2021-03-23 17:00:04 +0700

    Change `ColDef` to `GridColDef` for @material-ui/data-grid

--- src/components/SessionList/hooks/session-data.tsx
+++ src/components/SessionList/hooks/session-data.tsx
@@ -51,6 +51,8 @@ interface ListSessionSummaryOptions {
 }
 
 export function useSessionData ({ apiKey, startTime, endTime }: SessionDataOptions) {
+  console.log(startTime, endTime);
+
   const [sessions, setSessions] = useState<SessionData[]>([]);
   const [endCursor, setEndCursor] = useState<string | undefined>();
   const { loading, error, data, fetchMore } = useQuery<any, ListSessionSummaryOptions>(LIST_SESSION_SUMMARY, {
